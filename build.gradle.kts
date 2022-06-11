6ad4f0e039a5120353f56fdfee3f1cec5a9344e5
mooziii <63669478+mooziii@users.noreply.github.com>
2022-06-11 16:27:59 +0200

    Keep dependencies up to date

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("io.papermc.paperweight.userdev") version "1.3.5"
+    id("io.papermc.paperweight.userdev") version "1.3.6"
     id("xyz.jpenilla.run-paper") version "1.0.6"
 }
 
@@ -12,7 +12,7 @@ repositories {
 }
 
 dependencies {
-    paperDevBundle("1.18.1-R0.1-SNAPSHOT")
+    paperDevBundle("1.19-R0.1-SNAPSHOT")
 }
 
 tasks {
